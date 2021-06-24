bdb8325bc7d224a181f7714018b9276c57771c6d
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-23 22:26:30 -0400

    added value and date

--- models/transaction.js
+++ models/transaction.js
@@ -7,8 +7,17 @@ const transactionSchema = new Schema(;
   type: String,
   trim: true,
   required: "Enter a name for this transaction"
-
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+    }
   }
-
+);
+cpmst Tramsactopm
 
 }
