91c72e58676ae2b8086a4befc733f709c03aac06
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-23 22:23:59 -0400

    working on transactionSchema

--- models/transaction.js
+++ models/transaction.js
@@ -1,4 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const transactionSchema = new
+const transactionSchema = new Schema();
+{
+  name: {
+  type: String,
+  trim:true
+  required: "Enter a name for this transaction"
+
+  }
+
+
+}
